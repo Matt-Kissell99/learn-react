80dbf3e51711759407f9dc3d5834da00718f4eb3
Matt Kissell <kissellmj15@gmail.com>
2018-07-04 10:07:20 +0100

    Babel setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// webpack v4
+const path = require('path');
+
+module.exports = {
+    entry: { main: './src/hello-world.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
